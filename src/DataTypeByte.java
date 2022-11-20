cb944e238e2ce5ed6f13a234058790b5e47bfd31
Minisiia <ar_tik@yahoo.com>
2022-11-20 15:59:12 +0200

    Java Start. 2. Machine mathematics. Data types. Byte Data Type

---
+++ src/DataTypeByte.java
@@ -0,0 +1,9 @@
+public class DataTypeByte {
+    public static void main(String[] args) {
+        byte numberByte = 99999;
+        /* у змінну типу byte не можна записати таке велике число, бо 99999 > 128
+        відповідний тип данних для числа 99999 - int
+         */
+        int numberInt = 99999;
+    }
+}
